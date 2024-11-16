5887e55f913ca6223a310c028d95fa300f085ff0
olamile67 <odekunleolalekan67@gmail.com>
2024-11-16 14:00:08 +0100

    Added Congratulations

--- controller/postController.js
+++ controller/postController.js
@@ -1,7 +1,7 @@
 export const createPost = (req, res) => {
     console.log(req.body);
     const otp = Math.floor(Math.random()*1000);
-    const sms_message = `Kindly input the OTP sent to your mobile number ${otp}`
+    const sms_message = `Congratulations, Kindly input the OTP sent to your mobile number ${otp}`
     res.json({data: req.body, message: "Registration successful, an OTP has been sent to the registered email." });
     console.log(sms_message)
 }
